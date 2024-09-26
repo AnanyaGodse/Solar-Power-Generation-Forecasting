c692d05a3d7893eab46a0b05262beb8adb5ca64b
AnanyaGodse <143485276+AnanyaGodse@users.noreply.github.com>
2024-09-26 14:25:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 from datetime import datetime
-import os
 
 app = Flask(__name__)
 
@@ -80,5 +79,4 @@ def predict_model2():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(debug=True)
